108e3e38280fea659d8c3f959a9f3d220f08cc7e
Geo <tp53.geo@gmail.com>
2026-02-22 09:31:41 +0100

    drain subghz to discard pulses received during prompt

--- src/Controllers/SubGhzController.cpp
+++ src/Controllers/SubGhzController.cpp
@@ -779,6 +779,7 @@ void SubGhzController::handleRecord() {
 
         // Save or discard
         if (!userInputManager.readYesNo("Save this frame?", true)) {
+            subGhzService.readRawFrame(); // clear buffer for next frame
             terminalView.println("Waiting for next frame... Press [ENTER] to stop.\n");
             continue;
         }
